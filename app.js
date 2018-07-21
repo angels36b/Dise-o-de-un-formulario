44ecf9cd7246186381a79d294bcc0e601f63fc2d
MECATRONICO\ANGEL <angel.36b@gmail.com>
2018-07-20 22:19:35 -0500

    la Instruccion de reseteo fue agregado

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-class product{ //clase para el producto
+class Product{ //clase para el producto
     constructor(name, price, year){ //metodo contructor
    this.name= name;
    this.price= price;
@@ -7,18 +7,29 @@ class product{ //clase para el producto
 }
 
 class UI{ //clase de la interfaz. la iterfaz accede al html
-    addproduct(producte){
+    addproduct(product){
        const productList = document.getElementById('product-list');
-       const element = document.createElement("div"); //dentro del div agrefo el producto
-       element.innerHTML = "<div class='card text-center mb-4'> <div class='card-body'> <strong> product Name </strong>:${product.name}  <strong> product Price </strong> :${product.price} <strong> product Year </strong> :${product.year} </div>  </div>  " ;
+       const element = document.createElement('div'); //dentro del div agrefo el producto
+       element.innerHTML="<div class ='card text-center mb-4'> <div class='card-body'> <strong> product Name </strong>: "+product.name+" <strong> product Name </strong> : "+product.price+"  <strong> product Price </strong> : "+product.year +" <a href='#' class='btn btn-danger' name=delate>Delate</a>   </div> </div> ";
+             
+             //  
+             //  ///Llama el objeto producto
+              
+              //
+        
+        
+        //<strong> product Year </strong> :${product.year} 
        productList.appendChild(element)
+       this.resetform();
        
-       
-       
+    
 
       
     }//esta clase viene de boostrap, el contenido estara centrado y cada tarjeta tendra un marjen de 4
-    
+    resetform(){
+        document.getElementById('product-form').reset();
+    }
+
     deleteProduct(){
 
     }
@@ -35,10 +46,13 @@ document.getElementById('product-form').addEventListener('submit', function(evit
 const name = document.getElementById('name').value;
 const price = document.getElementById('price').value;
 const year = document.getElementById('year').value;
-console.log(name,price,year);
-const producte (new product(name, price, year)); //los datos creados se convierten en un objeto unico
+//console.log(name,price,year);
 const ui = new UI(); //Creamos una nueva instancia de la clase ui
-ui.addproduct(producte);
+const product=(new Product(name, price, year)); //los datos creados se convierten en un objeto unico
+ui.addproduct(product); 
 evita.preventDefault(); //evita el refrescamiento por defecto de la pagina
+
 })
-     
+
+
+
